1f87d903c3e8ad4795accba10fb04686039b97b8
LisaHuang <1449700087@qq.com>
2018-03-29 17:15:18 +0800

    提交section3/PracticeC.java的实现代码

--- src/main/java/cn/school/thoughtworks/section3/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeC.java
@@ -1,12 +1,37 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeC {
+
+    private static final int SPECIAL_NUMBER = 3;
+
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> collection3 = new HashMap<>();
 
-        return null;
+        Set<String> uniqueSet = new HashSet<>(collectionA);
+        for (String setElement: uniqueSet) {
+
+            int count = Collections.frequency(collectionA,setElement);
+            if(count > 1) {
+                collection3.put(setElement, count);
+
+            }
+        }
+
+        List<String> objectValue = object.get("value");
+        for (int i = 0; i < objectValue.size(); i++) {
+
+            if(collection3.containsKey(objectValue.get(i)) == true) {
+
+                if (collection3.get(objectValue.get(i)) >= SPECIAL_NUMBER) {
+                    collection3.put(objectValue.get(i), collection3.get(objectValue.get(i))-collection3.get(objectValue.get(i))/ SPECIAL_NUMBER);
+                }
+
+            }
+        }
+
+        return collection3;
     }
 }

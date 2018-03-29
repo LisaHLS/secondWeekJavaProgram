748a06c78c3718cebb20bb7a0dba4c918e6dfdac
LisaHuang <1449700087@qq.com>
2018-03-29 17:03:27 +0800

    提交section2/PracticeA.java的实现代码

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,21 @@
 package cn.school.thoughtworks.section2;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> result = new HashMap<>();
 
-        return null;
+        Set<String> uniqueSet = new HashSet<>(collection1);
+        for (String setElement: uniqueSet) {
+
+            int count = Collections.frequency(collection1,setElement);
+            if(count > 1) {
+                result.put(setElement, count);
+            }
+
+        }
+        return result;
     }
 }

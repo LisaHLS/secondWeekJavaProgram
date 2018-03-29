fa05d3bf17f18c839c4cb3399ab51ebe4acaba2a
LisaHuang <1449700087@qq.com>
2018-03-29 16:57:37 +0800

    提交section1/PracticeB.java的实现代码

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> sameElementsCollection = new ArrayList<>(collection1);
+
+        for (int i = 0; i <collection2.size() ; i++) {
+            sameElementsCollection.retainAll(collection2.get(i));
+        }
+
+        return sameElementsCollection;
     }
 }

cbdb2543f51c93715b8f1a8847c687d481a4e732
LisaHuang <1449700087@qq.com>
2018-03-29 17:11:15 +0800

    提交section3/PracticeA.java的实现代码

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -6,7 +6,13 @@ import java.util.Map;
 public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> objectValue = object.get("value");
+        for (int i = 0; i < objectValue.size(); i++) {
 
-        return null;
+            if(collectionA.containsKey(objectValue.get(i)) == true) {
+                collectionA.put(objectValue.get(i), collectionA.get(objectValue.get(i))-1);
+            }
+        }
+        return collectionA;
     }
 }

64097e5904ea7836191fb0c6a887101776bc9b1d
LisaHuang <1449700087@qq.com>
2018-03-29 17:13:13 +0800

    提交section3/PracticeB.java的实现代码

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,22 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
+
+    private static final int SPECIAL_NUMBER = 3;
+
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        List<String> objectValue = object.get("value");
+        for (int i = 0; i < objectValue.size(); i++) {
 
-        return null;
+            if(collectionA.containsKey(objectValue.get(i)) == true) {
+
+                if (collectionA.get(objectValue.get(i)) >= SPECIAL_NUMBER) {
+                    collectionA.put(objectValue.get(i), collectionA.get(objectValue.get(i))-collectionA.get(objectValue.get(i))/ SPECIAL_NUMBER);
+                }
+            }
+        }
+
+        return collectionA;
     }
 }
